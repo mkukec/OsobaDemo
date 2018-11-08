b4e2bf9715c309d4dd40cc001226d1593c900232
Mihael <mihael.kukec@mev.hr>
2018-11-08 07:48:59 +0100

    Razred Osoba, metode za dohvat i postavljanje podataka Ime, Prezime, OIB

--- OsobaDemo/Osoba.cs
+++ OsobaDemo/Osoba.cs
@@ -10,6 +10,42 @@ namespace OsobaDemo
         private string Ime;
         private string Prezime;
         private string OIB;
+
+        // Metoda za postavljanje podatka 'Ime'
+        public void postaviIme(string Ime)
+        {
+            this.Ime = Ime;
+        }
+
+        // Metoda za dohvat podataka 'Ime'
+        public string dohvatiIme()
+        {
+            return Ime;
+        }
+
+        // Metoda za postavljanje podataka 'Prezime'
+        public void postaviPrezime(string Prezime)
+        {
+            this.Prezime = Prezime;
+        }
+
+        // Metoda za dohvat podataka 'Prezime'
+        public string dohvatiPrezime()
+        {
+            return Prezime;
+        }
+
+        // Metoda za postavljanje podataka 'OIB'
+        public void postaviOIB(string OIB)
+        {
+            this.OIB = OIB;
+        }
+
+        // Metoda za dohvat podataka 'OIB'
+        public string dohvatiOIB()
+        {
+            return OIB;
+        }
         
         public void ispisiPrezimeIme()
         {
